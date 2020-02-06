20a9ceff299e7b8b83bb759af1cf0efe9aecd4d3
mango_glb <goodMorning_glb@atguigu.com>
2020-02-06 20:55:10 +0800

    实现前后端登录简单逻辑

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,7 +1,64 @@
 <template>
-  <div>登录</div>
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      label-position="left"
+      label-width="80px"
+      :model="formdata"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button
+        @click.prevent="handleLogin()"
+        class="login-button"
+        type="primary"
+        >登录</el-button
+      >
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$http.post('/login', this.formdata).then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
 </script>
+
+<style>
+.login-wrap {
+  height: 100%;
+  background-color: blanchedalmond;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-wrap .login-form {
+  width: 400px;
+  background-color: gold;
+  border-radius: 5px;
+  padding: 30px;
+}
+
+.login-wrap .login-button {
+  width: 100%;
+}
+</style>
