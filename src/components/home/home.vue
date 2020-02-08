e254a99f49797b485fdd4257114e888eef421c33
mango_glb <goodMorning_glb@atguigu.com>
2020-02-08 16:35:02 +0800

    引入侧边栏，调整菜单

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,19 +2,84 @@
   <el-container class="container">
     <el-header class="header">
       <el-row>
-        <el-col :span="4"><div class="grid-content bg-purple">
-            <img src="../../assets/msld.jpg" alt="无法显示图片">
-        </div></el-col>
+        <el-col :span="4"
+          ><div class="grid-content bg-purple">
+            <img src="../../assets/msld.jpg" alt="无法显示图片" /></div
+        ></el-col>
         <el-col :span="18" class="middle">
-            <h3>租车后台管理系统</h3>
+          <h3>租车后台管理系统</h3>
         </el-col>
-        <el-col :span="2"><div class="grid-content bg-purple">
+        <el-col :span="2"
+          ><div class="grid-content bg-purple">
             <a class="loginout" href="#">退出</a>
-        </div></el-col>
+          </div></el-col
+        >
       </el-row>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-aside width="200px" class="aside">
+        <el-menu :unique-opened="true">
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+                <i class="el-icon-menu"></i>
+                <span>权限列表</span>
+              </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>汽车管理</span>
+            </template>
+            <el-menu-item index="3-1">
+              <i class="el-icon-menu"></i>
+              <span>选项1</span>
+            </el-menu-item>
+          </el-submenu>
+          <!--4-->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+              <i class="el-icon-menu"></i>
+              <span>选项1</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">
+              <i class="el-icon-menu"></i>
+              <span>选项1</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -38,11 +103,11 @@ export default {}
   background-color: #e9eef3;
   height: 100%;
 }
-.middle{
-    text-align: center;
+.middle {
+  text-align: center;
 }
-.loginout{
-    line-height: 60px;
-    text-decoration: none;
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
