206932d3ef41eda4a0992a6ea1a0f75abb24be1c
mango_glb <goodMorning_glb@atguigu.com>
2020-02-11 23:52:53 +0800

    表格获取数据，编辑和状态按钮设置

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -16,22 +16,44 @@
       </el-rol>
     </el-row>
     <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="userList" style="width: 100%">
       <el-table-column type="index" label="#" width="60"> </el-table-column>
-      <el-table-column prop="name" label="用户名" width="80"> </el-table-column>
-      <el-table-column prop="name" label="密码" width="80"> </el-table-column>
-      <el-table-column prop="name" label="盐" width="80"> </el-table-column>
-      <el-table-column prop="name" label="角色" width="80"> </el-table-column>
+      <el-table-column prop="username" label="用户名" width="80">
+      </el-table-column>
+      <el-table-column prop="password" label="密码" width="80">
+      </el-table-column>
+      <el-table-column prop="salt" label="盐" width="80"> </el-table-column>
+      <el-table-column prop="roles" label="角色" width="80"> </el-table-column>
       <el-table-column prop="name" label="姓名" width="80"> </el-table-column>
-      <el-table-column prop="name" label="性别" width="60"> </el-table-column>
-      <el-table-column prop="name" label="手机号码"> </el-table-column>
-      <el-table-column prop="name" label="身份证"> </el-table-column>
-      <el-table-column prop="name" label="邮箱"> </el-table-column>
-      <el-table-column prop="name" label="银行卡"> </el-table-column>
+      <el-table-column prop="sex" label="性别" width="60"> </el-table-column>
+      <el-table-column prop="telephone" label="手机号码"> </el-table-column>
+      <el-table-column prop="idcard" label="身份证"> </el-table-column>
+      <el-table-column prop="email" label="邮箱"> </el-table-column>
+      <el-table-column prop="bankcard" label="银行卡"> </el-table-column>
       <el-table-column prop="address" label="常驻地址"> </el-table-column>
-      <el-table-column prop="name" label="创建日期"> </el-table-column>
-      <el-table-column prop="name" label="用户状态"> </el-table-column>
-      <el-table-column prop="name" label="操作"> </el-table-column>
+      <el-table-column prop="userCreateTime" label="创建日期">
+        <template slot-scope="userList">
+          <!--  userList.row.userCreatetime userlist的每个对象 会经度有误差 -->
+          {{ userList.userCreateTime | fmtdate }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="userStatus" label="用户状态">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.userStatus"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          >
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+          <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -41,18 +63,7 @@ export default {
   data () {
     return {
       query: '',
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }
-      ]
+      userList: ''
     }
   },
   created () {
@@ -60,12 +71,15 @@ export default {
   },
   methods: {
     async getUserList () {
-      // 需要授权的API,必须在请求头中使用后端定义的Authorization字段提供token令牌
-    //  const AUTH_TOKEN =localStorage.getItem('token')
-    //  this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
-
-      const res = await this.$http.get('/user/selectall?参数')
-      console.log(res)
+      //  需要授权的API,必须在请求头中使用后端定义的Authorization字段提供token令牌
+      //  const AUTH_TOKEN =localStorage.getItem('token')
+      //  this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
+      const res = await this.$http.get('/user/selectall')
+      console.log(res.data)
+      if (res.data.code === 'SUCCESS') {
+        this.userList = res.data.data
+        this.$message.success('ok')
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import '@/assets/css/base.css'
 import MyHttpServer from '@/plugins/http.js'
+import moment from 'moment'
+
+import '@/assets/css/base.css'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(MyHttpServer)
+
+// 全局过滤器
+Vue.filter('fmtdate', v => {
+  return moment(v).format('YYYY-MM-DD')
+})
 
 /* eslint-disable no-new */
 new Vue({
